c4c8d47f9ee28548a9e5ccf6895b3cc08ad1c1b0
Edgar <edguitian@imagine800.es>
2023-12-06 17:19:38 +0100

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Edgar Guitian Rey on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
